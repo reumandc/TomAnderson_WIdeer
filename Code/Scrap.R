438bb2ad8e61d532b58eda256c8cc5c702ede544
Dan Reuman <reuman@ku.edu>
2018-04-23 10:36:23 -0500

    trying to get checkpoint to work

--- Code/Scrap.R
+++ Code/Scrap.R
@@ -1,18 +1,13 @@
-<!--checkpoint package for CRAN-available packages
-```{r checkpoint_chunk, echo=F, warning=F, message=F, results="hide"}
-library(checkpoint)
-if (!dir.exists("./.checkpoint/")){
-  dir.create("./.checkpoint/")
-}
-checkpoint("2018-04-20",checkpointLocation = "./")
-```
--->
-  
+
 
 Things to do:
 D - checkpoint and install_github need to play together, somehow
 T - set.seed in appropriate places make it exactly reproducible
 D - put pedagog fig code in
 DT - use cacheing, which means sorting out dependencies
-sort out warnings, don't just supress them
-ready to add data to repo?
+T - sort out warnings, don't just supress them
+? - ready to add data to repo?
+T - Tom to provide info as indicated in comments so Dan can complete 
+examples of all the ways R code outputs and other content are automatically 
+linked in and Rmd (figs, tables, inline numeric values stored in R variables, 
+citations, etc.)
