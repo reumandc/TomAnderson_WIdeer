bcc58ef70ee55a6b331a5ddbdca69bea3bda087e
Dan Reuman <reuman@ku.edu>
2018-04-25 16:37:31 -0500

    added example citation using the Rmd reference management system, on line 226

--- Code/Scrap.R
+++ Code/Scrap.R
@@ -8,3 +8,12 @@ T - Tom to provide info as indicated in comments so Dan can complete
 examples of all the ways R code outputs and other content are automatically 
 linked in and Rmd (figs, tables, inline numeric values stored in R variables, 
 citations, etc.)
+
+
+#Cut this from the .bib file - it did not have a tag
+@misc{
+   author = {Interior, U.S. Department of the and Service, U.S. Fish and Wildlife and U.S. Department of Commerce, U.S. Census Bureau},
+   title = {National Survey of Fishing, Hunting, and Wildlife-Associated Recreation},
+   year = {2011},
+   type = {Government Document}
+}
