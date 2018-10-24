f8026879c258cb5b14cef2524b18334cda4236b4
anderstl <anderstl@gmail.com>
2018-10-24 15:26:15 -0500

    added normalization function and updated deer plots

---
+++ Functions/Fn_wavnorm.R
@@ -0,0 +1,14 @@
+#Function for normalizing wavelet transforms of a time series
+#
+#Args
+#wav  --wavelet transforms from a call to wt() function from the 'wsyn' package. Should be
+#       in time X timescale format
+#
+#Output
+##a matrix containing power normalized wavelet tranforms (i.e., wavelet transforms divided
+##by the square root of the time-averaged squared wavelet transform magnitudes)
+wavnorm<-function(wav){
+  modwav <- Mod(wav)
+  denom<-sqrt(apply(wav,2,mean,na.rm=T))
+  normwav<-sweep(modwav^2,2,denom,`/`)
+}
