2f516f0f32b9f64ed12534fdc3e85729780f2c7e
anderstl <anderstl@gmail.com>
2019-01-25 16:43:03 -0600

    finished commenting on steps

--- Code/AltHypo_DeerCycles.R
+++ Code/AltHypo_DeerCycles.R
@@ -1,14 +1,17 @@
 #set working directory
 setwd("C:/Users/Tom/Documents/GitRepos/TomAnderson_WIdeer")
 
+#load wsyn
+library(wsyn)
+
 #load in raw data deer data
-cty.list<-read.rds("Results/wlm_abun.rds")
+cty.list<-readRDS("Results/cty.list.rds")
 
 #clean deer abundance data
-deer.clean<-cleandat(cty.list$Abun,clev=5,times=1981:2016)
+deer.clean<-cleandat(cty.list$Abun,clev=5,times=1981:2016)$cdat
 
 #load in raw PDO/MEI data
-climindex<-read.rds("Results/climindex.rds")
+climindex<-readRDS("Results/climindex.rds")
 
 #clean climate index data
 climindex.clean<-lapply(climindex,function(x){x<-cleandat(x,times=1981:2016,clev=5)$cdat;x})
@@ -16,17 +19,22 @@ pdo.clean<-climindex.clean$WinterPDO
 mei.clean<-climindex.clean$WinterMEI
 
 #load in raw snow depth data
-winter.clim<-read.rds("Results/winter.clim.rds")
+winter.clim<-readRDS("Results/winter.clim.rds")
 
 #drop rows with NAs in snow data
 snow.tmp<-winter.clim[['Snwd']][!is.na(rowMeans(winter.clim[['Snwd']])),]
 
 #clean snow data
-snow.clean<-cleandat(snow.tmp,clev=5,times=minyear:maxyear)$cdat
+snow.clean<-cleandat(snow.tmp,clev=5,times=1981:2016)$cdat
 
 #drop rows from deer data set where snow depth had NAs so that spatial dimensions match
 #note: would need to repeat this process on PDO and MEI to run wlm models so that they all match in spatial extent
 deer.clean_snowsubset<-deer.clean[!is.na(rowMeans(winter.clim[['Snwd']])),]
 
 #load in wavelet linear model of deer abundance (predictors snow depth, PDO and MEI)
-wlm_abun<-read.rds("Results/wlm_abun.rds")
+wlm_abun<-readRDS("Results/wlm_abun.rds")
+
+snow_deer_coherence<-coh(dat1=deer.clean_snowsubset,dat2=snow.clean,times=1981:2016,norm="powall",
+                                sigmethod="fast",nrand=100000,f0=1)
+coh_4to7<-snow_deer_coherence$coher[snow_deer_coherence$timescales>4 & snow_deer_coherence$timescales<7]
+
