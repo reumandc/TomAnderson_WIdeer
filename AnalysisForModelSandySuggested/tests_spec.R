b8a1781017ca76c0848a16d58dc994b04a759e47
Dan Reuman <reuman@ku.edu>
2019-06-25 10:22:57 -0500

    initial try at some examples, not good

---
+++ AnalysisForModelSandySuggested/tests_spec.R
@@ -0,0 +1,61 @@
+#***libraries and other pre work
+
+rm(list=ls())
+library(mvtnorm)
+source("my.spec.pgram.R")
+
+#***see if the raw spectrum, summed, equals the variance
+
+#get some data
+set.seed(101)
+lents<-1024
+x<-rnorm(lents)
+#x<-x-mean(x)
+
+#take spectra in two ways to compare - should be the same
+pg<-my.spec.pgram(x,detrend=FALSE,plot=FALSE,taper=0)
+length(pg$spec)
+y<-((Mod(fft(x)))^2)/lents
+y<-y[2:length(y)]
+ycut<-y[1:(length(pg$spec))]
+cbind(ycut,pg$spec)
+max(abs(ycut-pg$spec))
+#so, without smoothing or tapering, spec.pgram gives 
+#((Mod(fft))^2)/lents (with the 0 frequency removed
+#and the redundant half of the spectrum removed)
+
+#compare to the variance
+var(x)*(lents-1)/lents #use the biased estimator of variance
+sum(y)/lents
+sum(y)/lents-(var(x)*(lents-1)/lents)
+
+#now use the unbiased estimator
+var(x)
+sum(y)/(lents-1)
+var(x)-sum(y)/(lents-1)      
+
+#***now look at covariance and the cospectrum
+
+#get some data
+set.seed(101)
+lents<-1024
+rho<-.8
+Sig<-matrix(c(1,.8,.8,1),2,2)
+x<-rmvnorm(lents,mean=c(0,0),sigma=Sig)
+
+#take cross spectra in two ways to compare - should be the same
+pg<-my.spec.pgram(x,detrend=FALSE,plot=FALSE,taper=0)
+dim(pg$pgram)
+y<-(fft(x[,1])*Conj(fft(x[,2])))/lents
+y<-y[2:length(y)]
+ycut<-y[1:(dim(pg$pgram)[1])]
+cbind(ycut,pg$pgram[,1,2])
+max(Mod(ycut-pg$pgram[,1,2]))
+#so, without smoothing or tapering, spec.pgram gives 
+#(fft(x[,1])*Conj(fft(x[,2])))/lents (with the 0 frequency removed
+#and the redundant half of the cross spectrum removed)
+
+#compare to the covariance, go straight to the unbiased estimator
+cov(x[,1],x[,2])
+sum(y)/(lents-1)
+cov(x[,1],x[,2])-Re(sum(y)/(lents-1))
